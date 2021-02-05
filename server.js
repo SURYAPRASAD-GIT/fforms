aab1e5c87e3657eec2a5801c075d7d7b1c77aebe
Surya <baddisuryanagaprasad@gmail.com>
2021-02-05 16:17:07 +0530

    hi

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + './dist/ng-forms'));
-
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname +
+    '/dist/ng-forms/index.html'));});
 app.listen(process.env.PORT || 8080);
